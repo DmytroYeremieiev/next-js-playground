2f3b379d822a50c260f0cf4283d6b20c04c3a23d
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2021-01-05 11:58:08 +0200

    - debugging

--- local-server/app.js
+++ local-server/app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 4000;
 
 app.get("/cards", (req, res) => {
+  console.log("req", new Date(parseInt(req.query.timestamp)).toString());
   res.sendFile(path.join(__dirname, "cards.json"));
 });
 
--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -14,10 +14,12 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <div className={styles.grid}>
-          <a className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {props.cards.map((c) => (
+            <a key={c.id} className={styles.card}>
+              <h3>{c.title}</h3>
+              <p>{c.name}</p>
+            </a>
+          ))}
         </div>
       </main>
     </div>
@@ -25,10 +27,13 @@ export default function Home(props) {
 }
 
 export async function getStaticProps(props) {
-  const cards = (await axios.get("http://localhost:4000/cards")).cards;
+  const cards = (
+    await axios.get(`http://localhost:4000/cards?timestamp=${Date.now()}`)
+  ).data.cards;
   return {
     props: {
       cards,
     },
+    revalidate: 15,
   };
 }
